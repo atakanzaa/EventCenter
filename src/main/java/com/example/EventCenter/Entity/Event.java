99d8a328e76e844736215c099bed7ca043814318
batuacaar <batuacaar@gmail.com>
2024-11-24 18:51:55 +0300

    token tutulması, kayıt olunan user ile rol eşlenmesi, adminin etkinlik oluşturabilmesi, etkinlik id ile category id eşlenmesi, ara tabloların oluşması vb yapıldı

--- src/main/java/com/example/EventCenter/Entity/Event.java
+++ src/main/java/com/example/EventCenter/Entity/Event.java
@@ -3,6 +3,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
@@ -19,13 +21,22 @@ public class Event {
 
     private String title;
     private String description;
-    private String category;
+    private String categoryName;
     private String location;
     private LocalDateTime eventDate;
     private String status;
     private LocalDateTime createdAt;
     private Integer maxCapacity;
     private Integer minAge;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "event_category",
+            joinColumns = @JoinColumn(name = "event_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> event_categories = new HashSet<>();
+
 
     // Getters and Setters
 }
